3c9bb6b300786fef3ca19315629002e264644693
dahhou-ilyas <dahhou.ilyas@gmail.com>
2023-02-26 00:01:40 +0100

    frontEnd

---
+++ front-end/src/App.js
@@ -0,0 +1,44 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <header>
+        <a href="#" className="logo">MyBlog</a>
+        <nav>
+          <a href="#">Login</a>
+          <a href="#">Register</a>
+        </nav>
+      </header>
+      <div className="post">
+        <div className='image'>
+           <img src='https://www.pixelstalk.net/wp-content/uploads/images6/Anime-Boy-Wallpaper-Free-Download-768x432.jpg' alt=''/>
+        </div>
+        <div className='text'>
+            <h2>socano is the great man</h2>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className='image'>
+           <img src='https://www.pixelstalk.net/wp-content/uploads/images6/Anime-Boy-Wallpaper-Free-Download-768x432.jpg' alt=''/>
+        </div>
+        <div className='text'>
+            <h2>socano is the great man</h2>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble</p>
+        </div>
+      </div>
+      <div className="post">
+        <div className='image'>
+          <img src='https://www.pixelstalk.net/wp-content/uploads/images6/Anime-Boy-Wallpaper-Free-Download-768x432.jpg' alt=''/>
+        </div>
+        <div className='text'>
+            <h2>socano is the great man</h2>
+            <p>Le Lorem Ipsum est simplement du faux texte employé dans la composition et la mise en page avant impression. Le Lorem Ipsum est le faux texte standard de l'imprimerie depuis les années 1500, quand un imprimeur anonyme assembla ensemble</p>
+        </div>
+      </div>
+    </main>
+  );
+}
+
+export default App;

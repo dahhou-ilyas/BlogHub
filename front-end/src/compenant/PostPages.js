0b23944ec091dcea0ccb23d0f2f57bb807e5ebd9
dahhou-ilyas <dahhou.ilyas@gmail.com>
2023-03-31 16:04:03 +0000

    add css

--- front-end/src/compenant/PostPages.js
+++ front-end/src/compenant/PostPages.js
@@ -1,3 +1,4 @@
+import { formatISO, formatISO9075 } from 'date-fns';
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import Post from './Post';
@@ -17,11 +18,14 @@ function PostPages() {
     if(!postInfo) return ''
     return (
       <div className='post-page'>
+       <h1>{postInfo.titre}</h1>
+       <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+       <div className='author'>by @{postInfo.author.username}</div>
         <div className='image'>
           <img src={`http://localhost:4000/${postInfo.image}`}/>
         </div>
-        <h1>{postInfo.titre}</h1>
-        <div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
+        
+        <div className='contente' dangerouslySetInnerHTML={{__html:postInfo.content}}/>
       </div>
     )
 }
